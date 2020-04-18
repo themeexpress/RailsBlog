309901dc5a865d096f96cd2d04179111b08540e0
themeexpress <kamrul>
2020-04-18 15:14:37 +0900

    show, edit, listing and back link

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,16 +9,26 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save 
       flash[:notice] = "Successfully created..."
-      redirect_to article_path
+      redirect_to article_path(@article)
     else
       render :new
     end
   end
   def show
     @article=Article.find(params[:id])
-  
   end
-
+  def edit
+    @article=Article.find(params[:id])
+  end
+  def update
+    @article=Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice]="Article update successfully"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
   private 
     def article_params
       params.require(:article).permit(:title, :description)
