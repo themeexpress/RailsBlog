29fdc70b3d63deaf74a8dea05c234745ac5373bb
themeexpress <kamrul>
2020-05-01 02:00:00 +0900

    logout and login logically set and before_action metho set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  #Authentication methods
+  helper_method :current_user,:logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You Must be logged in to perform this action"
+      redirect_to root_path
+    end
+  end
+  
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  
+  before_action :set_article, only: [:show,:edit,:update,:destroy]
+  
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
@@ -16,13 +19,10 @@ class ArticlesController < ApplicationController
     end
   end
   def show
-    @article=Article.find(params[:id])
   end
   def edit
-    @article=Article.find(params[:id])
   end
   def update
-    @article=Article.find(params[:id])
     if @article.update(article_params)
       flash[:success]="Article update successfully"
       redirect_to article_path(@article)
@@ -31,7 +31,6 @@ class ArticlesController < ApplicationController
     end
   end
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:danger] = "Successfully deleted"
     redirect_to articles_path
@@ -40,4 +39,9 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
+    
+    def set_article
+      @article = Article.find(params[:id])
+    end
+    
 end
