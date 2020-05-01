e6ef6f29bda178b9de1ef1a8f2d7cc770fc25f15
themeexpress <kamrul>
2020-05-02 00:43:21 +0900

    complete authentication and update current user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only: [:show,:edit,:update,:destroy]
+  before_action :require_user, except: [:index]
+  before_action :require_same_user, only: [:edit, :update, :destory]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -10,7 +12,7 @@ class ArticlesController < ApplicationController
   end
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save 
       flash[:success] = "Successfully created..."
       redirect_to article_path(@article)
@@ -42,6 +44,13 @@ class ArticlesController < ApplicationController
     
     def set_article
       @article = Article.find(params[:id])
+    end
+    
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit your own post"
+        redirect_to root_path
+      end
     end
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit,:update,:show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -47,6 +48,15 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
+
+    def require_same_user
+      if current_user != @user
+        flash[:danger]="You can only edit your own account"
+        redirect_to root_path
+      end
+      
+    end
+    
     
   
 
